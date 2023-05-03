c5c0eb245106e093233832aad89b223042a5e123
KFGR <figueroa.8254@gmail.com>
2023-05-03 09:12:44 -0400

    .

--- Backend/CONFIG/connection.py
+++ Backend/CONFIG/connection.py
@@ -4,15 +4,15 @@ from sqlalchemy.orm import sessionmaker
 import os
 import json
 
-with open("Backend/CONFIG/config.json") as f:
-    config = json.load(f)
+# with open("Backend/CONFIG/config.json") as f:
+#     config = json.load(f)
 
-username = config["database_username"]
-password = config["database_password"]
-hostname = config["database_hostname"]
-port = config["database_port"]
-dbname = config["database_name"]
-pem_file = config["database_pem"]
+username = os.environ.get("datbase_username")#config["database_username"]
+password = os.environ.get("datbase_password")#config["database_password"]
+hostname = os.environ.get("datbase_hostname")#config["database_hostname"]
+port = os.environ.get("datbase_port")#config["database_port"]
+dbname = os.environ.get("datbase_name")#config["database_name"]
+pem_file = os.environ.get("database_pem")#config["database_pem"]
 ssl_arg = {"ssl_ca":pem_file}
 
 # Creating engine to connect to the database in Azure
