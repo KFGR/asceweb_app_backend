7854c35830b1f7017e1362f16e802f79869677bf
KFGR <figueroa.8254@gmail.com>
2023-05-16 15:13:42 -0400

    .

--- Backend/TESTS/Competitions_Test.py
+++ Backend/TESTS/Competitions_Test.py
@@ -26,7 +26,6 @@ def put_Competition_Data(db:Session, user:set_Competitions_Data):
     """
     if not ValidateExist(db,user=user):
         member_exist = db.query(Chapter_Members_Table).filter(Chapter_Members_Table.name == user.name, Chapter_Members_Table.email == user.email).first()
-        print(member_exist.email)
         if member_exist and member_exist.competitions_form != 'Yes' and member_exist.name == user.name and member_exist.email == user.email:        
             db_competitions = Competitions_Table(idchapter_members=member_exist.idchapter_members, name=member_exist.name, email=member_exist.email,phone=member_exist.phone, ascemembership=user.ascemembership, courses=user.courses, daily_avail=user.daily_availability, travel_avail=user.travel_availability,age_gt_twtfive=user.older_than_twentyfive, hv_vehicle=user.heavy_driver, offdriver_avail=user.official_driver, competitions_form=user.competitions_form, competition_name=user.competition_name, created_at=user.created_at, experiences=user.experiences, travel_june=user.travel_june, asce_member=user.asce_member)
             db.add(db_competitions)
